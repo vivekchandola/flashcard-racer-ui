1c58406e3d06e8bf908336f910a767823d2bdc1e
ClovsBob <clovis.bobillier@hotmail.fr>
2017-05-19 16:38:14 +0200

    second upload languages

--- flashcard-racer/app/controllers/LanguageController.java
+++ flashcard-racer/app/controllers/LanguageController.java
@@ -13,9 +13,6 @@ public class LanguageController extends Controller {
     
     
     public Result index(String language) {
-        /*if(language != null && !language.isEmpty()){
-            Controller.changeLang("fr");
-        }*/  
         Controller.changeLang(language);
         
         return redirect("/");
