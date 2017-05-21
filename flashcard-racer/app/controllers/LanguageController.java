5b88dbd4975d23758380938535a4219ed08140c0
ClovsBob <clovis.bobillier@hotmail.fr>
2017-05-21 18:37:41 +0200

    switch language done

--- flashcard-racer/app/controllers/LanguageController.java
+++ flashcard-racer/app/controllers/LanguageController.java
@@ -12,10 +12,13 @@ public class LanguageController extends Controller {
 
     
     
-    public Result index(String language) {
+    public Result index(String language, String address) {
         Controller.changeLang(language);
+        if(address.equals("0")){
+            return redirect("/");
+        }
         
-        return redirect("/");
+        return redirect("/"+address);
     }
 
  
